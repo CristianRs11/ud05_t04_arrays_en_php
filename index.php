1f4508d6c8a7e70b75ff240b3ec9e86ad61006a4
CristianRs11 <crodriguescalvo@gmail.com>
2022-01-15 22:40:54 +0100

    Array asociativo

--- index.php
+++ index.php
@@ -9,4 +9,9 @@
             //Array indexado
             $amigos=array("Antón","Rosalía","Xoán");
             echo "O terceiro amigo é: ".$amigos[2]."<br/>";
+            //Array asociativo
+            $dnisAmigos=array("Antón"=>"12345678x","Rosalía"=>"11112222x","Xoán"=>"99997777x");
+            echo "O DNI de Rosalía é: ".$dnisAmigos['Rosalía']."<br/>";
         ?>
+    </body>
+</html>
