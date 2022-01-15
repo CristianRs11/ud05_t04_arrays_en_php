0901af4c2cc482ca0b6401964c2231538cae62f6
CristianRs11 <crodriguescalvo@gmail.com>
2022-01-15 22:37:25 +0100

    Array indexado

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meat http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Definición de arrays en PHP</h3>
+        <?php
+            //Array indexado
+            $amigos=array("Antón","Rosalía","Xoán");
+            echo "O terceiro amigo é: ".$amigos[2]."<br/>";
+        ?>
